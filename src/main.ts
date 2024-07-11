e98f5dec4f9c08f6362986924ab5ecc0086621da
Andhika-GIT <andhikapr081@gmail.com>
2024-07-11 16:49:37 +0700

    feat: enable cors from certain development port

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { NotFoundExceptionFilter } from './common/exceptions/not-found-exception
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new NotFoundExceptionFilter());
+  app.enableCors({
+    allowedHeaders: ['content-type'],
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
   await app.listen(3001);
 }
 bootstrap();
