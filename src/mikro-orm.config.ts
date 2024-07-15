b11e59efc768d5d32a0ab2f22bf58b80319fc4f7
Andhika-GIT <andhikapr081@gmail.com>
2024-07-15 14:56:55 +0700

    feat: docker & mikro-ORM for deployment

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -5,23 +5,27 @@ import { TsMorphMetadataProvider } from '@mikro-orm/reflection';
 import { Migrator } from '@mikro-orm/migrations';
 import { SeedManager } from '@mikro-orm/seeder';
 import { User } from './user/entities/user.entity';
+import { ConfigService } from '@nestjs/config';
+
+const configService = new ConfigService();
 
 const config: Options = {
   entities: [Book, User],
-  dbName: 'bookstore',
-  user: 'postgres',
-  password: 'postgres',
-  host: 'localhost',
-  port: 5432,
+  dbName: configService.get<string>('DB_NAME', 'bookstore'),
+  user: configService.get<string>('DB_USER', 'postgres'),
+  password: configService.get<string>('DB_PASSWORD', 'postgres'),
+  host: configService.get<string>('DB_HOST', 'localhost'),
+  port: configService.get<number>('DB_PORT', 5432),
   driver: PostgreSqlDriver,
   metadataProvider: TsMorphMetadataProvider,
   seeder: {
-    path: './src/seeds',
+    path: configService.get<string>('SEEDER_PATH', './src/seeds'),
+    pathTs: configService.get<string>('SEEDER_PATH_TS', './src/seeds'),
     defaultSeeder: 'DatabaseSeeder',
   },
   migrations: {
-    path: './src/migrations',
-    pathTs: './src/migrations',
+    path: configService.get<string>('MIGRATIONS_PATH', './src/migrations'),
+    pathTs: configService.get<string>('MIGRATIONS_PATH_TS', './src/migrations'),
     glob: '!(*.d).{js,ts}',
   },
   extensions: [Migrator, SeedManager],
